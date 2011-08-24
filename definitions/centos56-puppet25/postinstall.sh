2907e0dae478cd6ebcbdbe6dca04ac832aa51d9c
Dan Carley <dan.carley@gmail.com>
2011-08-24 08:12:10 +0100

    Add `set -e` to postinstall. Remove relative paths and `cd` commands.

--- definitions/centos56-puppet25/postinstall.sh
+++ definitions/centos56-puppet25/postinstall.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 #http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
 
+set -e
 date > /etc/vagrant_box_build_time
 
 fail()
@@ -14,19 +16,17 @@ fail()
 # Installing vagrant keys.
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
-cd /home/vagrant/.ssh
-wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
+wget -P /home/vagrant/.ssh --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 # Installing the virtualbox guest additions.
 yum -y kernel-devel-`uname -r`
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-cd /tmp
-wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
-mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+wget -P /tmp http://download.virtualbox.org/virtualbox/${VBOX_VERSION}/VBoxGuestAdditions_${VBOX_VERSION}.iso
+mount -o loop /tmp/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm VBoxGuestAdditions_$VBOX_VERSION.iso
+rm /tmp/VBoxGuestAdditions_${VBOX_VERSION}.iso
 
 #poweroff -h
 yum -y clean all
