d521c9f70ad901222a512b5b819e3c42b2e64d6a
Dan Carley <dan.carley@gmail.com>
2011-09-02 13:53:35 +0100

    Fix authorized_keys filename.

--- definitions/centos60-puppet25/postinstall.sh
+++ definitions/centos60-puppet25/postinstall.sh
@@ -16,7 +16,7 @@ fail()
 # Installing vagrant keys.
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
-wget -P /home/vagrant/.ssh --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub'
+wget --no-check-certificate -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
 chown -R vagrant /home/vagrant/.ssh
 
 # Installing the virtualbox guest additions.
