a99601196ba6f662c3851e7fbfcd25c245b15190
Dan Carley <dan.carley@gmail.com>
2011-09-29 17:02:33 +0100

    Add mutlicast DNS definitions that use Avahi to do internal resolution.

---
+++ definitions/centos60-puppet26-mdns/postinstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
+
+set -e
+date > /etc/vagrant_box_build_time
+
+fail()
+{
+  echo "FATAL: $*"
+  exit 1
+}
+
+#yum -y update
+#yum -y upgrade
+
+# Installing vagrant keys.
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+wget --no-check-certificate -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+chown -R vagrant /home/vagrant/.ssh
+
+# Installing the virtualbox guest additions.
+yum -y install kernel-devel-`uname -r`
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+wget -P /tmp http://download.virtualbox.org/virtualbox/${VBOX_VERSION}/VBoxGuestAdditions_${VBOX_VERSION}.iso
+mount -o loop /tmp/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm /tmp/VBoxGuestAdditions_${VBOX_VERSION}.iso
+
+#poweroff -h
+yum -y clean all
+
+exit
